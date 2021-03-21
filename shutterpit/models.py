0f6bcd113a26778d3122aba4e48ecd572e9b2c8f
Eddy Chase <eddykanyz.ek@gmail.com>
2021-03-21 17:56:12 +0300

    addition of Image model

--- shutterpit/models.py
+++ shutterpit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 60)
+    pic = models.ImageField(upload_to = 'uploads/')
+    picture = ImageField( blank = True, manual_crop = '1920x1080')
+    description = models.TextField()
+    image_location = models.ForeignKey('Location')
+    image_category = models.ForeignKey('Category')
