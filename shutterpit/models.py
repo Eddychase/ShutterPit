603c3a8490753b866336441e042351726ced549e
Eddy Chase <eddykanyz.ek@gmail.com>
2021-03-21 18:01:37 +0300

    addition of save-image function

--- shutterpit/models.py
+++ shutterpit/models.py
@@ -10,6 +10,10 @@ class Image(models.Model):
     image_location = models.ForeignKey('Location')
     image_category = models.ForeignKey('Category')
 
+    def save_image(self):
+        self.save()
+
+
 class Location(models.Model):
     name = models.CharField(max_length =50)
 
