57d7bd01f83674ce7779c4d539d987072a403141
Eddy Chase <eddykanyz.ek@gmail.com>
2021-03-22 03:31:22 +0300

    fixed images not showing

--- shutterpit/models.py
+++ shutterpit/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length = 60)
-    pic = models.ImageField(upload_to = 'uploads/')
+    picture = models.ImageField(upload_to = 'uploads/')
     description = models.TextField()
     image_location = models.ForeignKey('Location',on_delete=models.CASCADE)
     image_category = models.ForeignKey('Category',on_delete=models.CASCADE)
@@ -46,6 +46,13 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+    
+    class Meta:
+        ordering = ['name']
+
+
 
 
 
@@ -100,6 +107,3 @@ class Category(models.Model):
 
     
     
-    class Meta:
-        ordering = ['name']
-
